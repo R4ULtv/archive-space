1fb5eaba619a8acca24d100e18e7daebbe3ed269
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-08-21 10:11:19 +0200

    add tags grid

---
+++ components/Tags.js
@@ -0,0 +1,43 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { Button } from "@headlessui/react";
+import { XMarkIcon } from "@heroicons/react/16/solid";
+
+export default function Tags({ tags, tag }) {
+  const router = useRouter();
+  const [filter, setFilter] = useState(tag);
+
+  const handleSubmit = (e) => {
+    if (filter === e) {
+      const url = new URLSearchParams();
+      router.push(`?${url.toString()}`);
+      return;
+    }
+    const url = new URLSearchParams();
+    url.set("tag", e);
+    router.push(`?${url.toString()}`);
+  };
+
+  useEffect(() => {
+    setFilter(tag);
+  }, [tag]);
+
+  return (
+    <div className="flex items-center justify-center gap-1.5 flex-wrap max-w-lg mx-auto">
+      {tags.map((tag) => (
+        <Button
+          key={tag}
+          onClick={() => handleSubmit(tag)}
+          className="flex items-center gap-0.5 bg-zinc-300 dark:bg-zinc-700 px-1.5 py-0.5 rounded text-xs text-zinc-900 dark:text-zinc-100"
+        >
+          {filter === tag && (
+            <XMarkIcon className="size-3 text-zinc-900 dark:text-zinc-100" />
+          )}
+          {tag}
+        </Button>
+      ))}
+    </div>
+  );
+}
---
+++ components/TagsGrid.js
@@ -0,0 +1,22 @@
+import clientPromise from "@/lib/mongodb";
+import Tags from "./Tags";
+
+export async function TagsGrid({ tag }) {
+  const client = await clientPromise;
+  const db = client.db("tests");
+  const tags = await db.collection("files").distinct("tags");
+
+  return <Tags tags={tags} tag={tag} />;
+}
+
+export function TagsGridLoading() {
+  return (
+    <div className="flex items-center justify-center gap-1.5 flex-wrap">
+      <div className="h-5 w-10 bg-zinc-300 dark:bg-zinc-700 rounded animate-pulse"></div>
+      <div className="h-5 w-12 bg-zinc-300 dark:bg-zinc-700 rounded animate-pulse"></div>
+      <div className="h-5 w-14 bg-zinc-300 dark:bg-zinc-700 rounded animate-pulse"></div>
+      <div className="h-5 w-10 bg-zinc-300 dark:bg-zinc-700 rounded animate-pulse"></div>
+      <div className="h-5 w-16 bg-zinc-300 dark:bg-zinc-700 rounded animate-pulse"></div>
+    </div>
+  );
+}
