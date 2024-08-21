0c755c5d2f006c3b114c0e13db5c7134bda80df4
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-08-21 10:10:57 +0200

    add file list

---
+++ components/FilesList.js
@@ -0,0 +1,112 @@
+import clientPromise from "@/lib/mongodb";
+import {
+  ArrowDownTrayIcon,
+  PencilSquareIcon,
+  TrashIcon,
+} from "@heroicons/react/20/solid";
+import Link from "next/link";
+import DownloadButton from "@/components/client/DownloadButton";
+import DeleteButton from "@/components/client/DeleteButton";
+import numeral from "numeral";
+import EditButton from "@/components/client/EditButton";
+
+export async function FilesList({ tag }) {
+  const client = await clientPromise;
+  const db = client.db("tests");
+  const files = await db
+    .collection("files")
+    .find(tag && { tags: tag })
+    .sort({ uploadedAt: -1 })
+    .toArray();
+
+  const categories = await db.collection("files").distinct("category");
+
+  return (
+    <div>
+      {files.map((file) => (
+        <div
+          key={file._id}
+          className="flex justify-between gap-4 py-1 sm:py-3 px-3 -mx-3 group rounded-md hover:bg-zinc-200 dark:hover:bg-zinc-800 duration-150"
+        >
+          <div className="flex-1">
+            <p className="font-medium text-zinc-800 dark:text-zinc-200">
+              {file.name}
+            </p>
+            <p className="text-xs text-zinc-600 dark:text-zinc-400 mt-0.5">
+              {file.lastModified.toLocaleDateString("en-US", {
+                month: "short",
+                day: "numeric",
+                year: "numeric",
+              })}{" "}
+              ·{" "}
+              {file.type.length > 24
+                ? file.type.substring(0, 24) + "..."
+                : file.type}{" "}
+              · {numeral(file.size).format("0.0 b")}
+            </p>
+            <div className="flex gap-1 mt-2">
+              {file.tags &&
+                file.tags.map((tag) => (
+                  <Link
+                    key={tag}
+                    href={`?tag=${tag}`}
+                    className="bg-zinc-300 dark:bg-zinc-700 px-1.5 py-0.5 rounded text-xs text-zinc-900 dark:text-zinc-100"
+                  >
+                    {tag}
+                  </Link>
+                ))}
+            </div>
+          </div>
+          <div className="flex items-center justify-center gap-1">
+            <EditButton
+              fileName={file.name}
+              category={file.category}
+              categories={categories}
+              oldTags={file.tags}
+            />
+            <DeleteButton fileName={file.name} />
+            <DownloadButton fileName={file.name} />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export function FilesListLoading() {
+  return (
+    <div>
+      {new Array(5).fill(0).map((_, i) => (
+        <div
+          key={i}
+          className="flex justify-between gap-4 py-1 sm:py-3 px-3 -mx-3 group rounded-md"
+        >
+          <div className="flex-1">
+            <div className="h-5 w-80 bg-zinc-300 dark:bg-zinc-700 rounded-lg animate-pulse"></div>
+            <div className="mt-1.5 flex gap-5">
+              <div className="h-3 w-20 bg-zinc-300 dark:bg-zinc-700 rounded-md animate-pulse"></div>
+              <div className="h-3 w-16 bg-zinc-300 dark:bg-zinc-700 rounded-md animate-pulse"></div>
+              <div className="h-3 w-12 bg-zinc-300 dark:bg-zinc-700 rounded-md animate-pulse"></div>
+            </div>
+            <div className="mt-3 flex gap-1">
+              <div className="h-5 w-12 bg-zinc-300 dark:bg-zinc-700 rounded animate-pulse"></div>
+              <div className="h-5 w-14 bg-zinc-300 dark:bg-zinc-700 rounded animate-pulse"></div>
+              <div className="h-5 w-10 bg-zinc-300 dark:bg-zinc-700 rounded animate-pulse"></div>
+            </div>
+          </div>
+          <div className="flex items-center justify-center gap-1 opacity-75">
+            <div className="p-1.5">
+              <PencilSquareIcon className="size-4 text-zinc-900 dark:text-zinc-100 animate-pulse" />
+            </div>
+            <div className="p-1.5">
+              <TrashIcon className="size-4 text-zinc-900 dark:text-zinc-100 animate-pulse" />
+            </div>
+            <div className="p-1.5">
+              <ArrowDownTrayIcon className="size-4 text-zinc-900 dark:text-zinc-100 animate-pulse" />
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
