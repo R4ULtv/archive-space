a9df5872a451cfd7a02c623a253fe44858422e90
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-08-23 16:30:47 +0200

    add category page

---
+++ app/(app)/[slug]/page.js
@@ -0,0 +1,34 @@
+import { notFound } from "next/navigation";
+import { Suspense } from "react";
+
+import SearchBar from "@/components/client/SearchBar";
+import { FilesList, FilesListLoading } from "@/components/FilesList";
+import { TagsGrid, TagsGridLoading } from "@/components/TagsGrid";
+import getCategories from "@/utils/getCategories";
+
+export async function generateStaticParams() {
+  const categories = await getCategories({ formatted: false });
+
+  return categories.map((category) => ({
+    slug: category,
+  }));
+}
+
+export default async function Blog({ params, searchParams }) {
+  const category = await getCategories({ formatted: false });
+
+  if (!category.includes(params.slug)) {
+    notFound();
+  }
+  return (
+    <div className="space-y-4 mt-4">
+      <SearchBar />
+      <Suspense fallback={<TagsGridLoading />}>
+        <TagsGrid tag={searchParams.tag} category={params.slug} />
+      </Suspense>
+      <Suspense fallback={<FilesListLoading />}>
+        <FilesList tag={searchParams.tag} category={params.slug} />
+      </Suspense>
+    </div>
+  );
+}
