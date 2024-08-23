cbe1f95d80241b5506495577a9095564f03db143
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-08-23 16:30:10 +0200

    add Delete File

---
+++ components/server/DeleteFile.js
@@ -0,0 +1,39 @@
+"use server";
+
+import { DeleteObjectCommand } from "@aws-sdk/client-s3";
+
+import { auth } from "@/lib/auth";
+import s3Client from "@/lib/s3Client";
+import clientPromise from "@/lib/mongodb";
+
+export async function DeleteFile(fileName) {
+  const session = await auth();
+  if (!session) {
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  }
+
+  const command = new DeleteObjectCommand({
+    Bucket: process.env.R2_BUCKET_NAME,
+    Key: fileName,
+  });
+
+  try {
+    const response = await s3Client.send(command);
+    if (!response) {
+      return { error: "The file was not deleted successfully." };
+    }
+
+    const client = await clientPromise;
+    const db = client.db("production");
+    const result = await db.collection("files").deleteOne({ name: fileName });
+
+    if (result.deletedCount === 0) {
+      return { error: "The record in the database was not deleted successfully." };
+    }
+    return { message: "File deleted successfully" };
+  } catch (error) {
+    return {
+      error: error.message,
+    };
+  }
+}
