749bf6398264062c730351a0f0bd16ee2250a065
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-08-25 17:20:21 +0200

    add multipart upload

--- components/client/UploadButton.js
+++ components/client/UploadButton.js
@@ -6,7 +6,10 @@ import { ArrowUpTrayIcon } from "@heroicons/react/20/solid";
 import { toast } from "sonner";
 import { useRouter } from "next/navigation";
 
-export default function UploadButton() {
+import saveFile from "@/components/server/SaveFile";
+import TokenGenerator from "@/components/server/TokenGenerator";
+
+export default function UploadButton({ fetchURL }) {
   const router = useRouter();
   const [loading, setLoading] = useState(false);
   const [dragActive, setDragActive] = useState(false);
@@ -17,28 +20,117 @@ export default function UploadButton() {
     fileInputRef.current.click();
   };
 
-  const uploadFile = async (file) => {
-    const formData = new FormData();
-    formData.append("file", file);
-    formData.append("lastModified", file.lastModified);
+  function calculateChunkSize(fileSize) {
+    const minChunkSize = 5;
+    const maxChunkSize = 95;
 
-    const response = await fetch("/api/upload", {
-      method: "POST",
-      body: formData,
-    });
+    const fileSizeMB = fileSize / (1024 * 1024);
 
-    if (!response.ok) {
-      toast.error("Something went wrong.", {
-        description: "Failed to upload file.",
+    let chunkSize = minChunkSize;
+
+    if (fileSizeMB <= 50) {
+      chunkSize = minChunkSize; // Small Files: fixed to 5 MB
+    } else if (fileSizeMB <= 300) {
+      chunkSize =
+        minChunkSize +
+        Math.floor(((fileSizeMB - 50) / (300 - 50)) * (30 - minChunkSize)); // Medium Files: from 5 MB up to 35 MB
+    } else if (fileSizeMB <= 1500) {
+      chunkSize =
+        35 +
+        Math.floor(((fileSizeMB - 300) / (1500 - 300)) * (maxChunkSize - 35)); // Big Files: from 35 MB up to 95 MB
+    } else {
+      chunkSize = maxChunkSize; // Very Big Files: fixed to 95 MB
+    }
+
+    return chunkSize * 1024 * 1024;
+  }
+
+  const uploadFile = async (file, token) => {
+    // Determine chunk size based on file size
+    const chunkSize = calculateChunkSize(file.size);
+    console.log(chunkSize);
+
+    const totalChunks = Math.ceil(file.size / chunkSize);
+    const uploadedParts = [];
+
+    // Step 1: Create a multipart upload
+    const createResponse = await fetch(
+      `${fetchURL}/${encodeURIComponent(file.name)}?action=mpu-create`,
+      {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    const { uploadId } = await createResponse.json();
+
+    for (let i = 0; i < totalChunks; i++) {
+      // Calculate start and end byte for each chunk
+      const start = i * chunkSize;
+      const end = Math.min(start + chunkSize, file.size);
+      const chunk = file.slice(start, end);
+
+      // Step 2: Upload each part
+      const uploadPartResponse = await fetch(
+        `${fetchURL}/${encodeURIComponent(
+          file.name
+        )}?action=mpu-uploadpart&uploadId=${uploadId}&partNumber=${i + 1}`,
+        {
+          method: "PUT",
+          mode: "cors",
+          body: chunk,
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      if (!uploadPartResponse.ok) {
+        toast.error(`Failed to upload chunk ${i + 1} of ${totalChunks}.`, {
+          description: "Something went wrong.",
+        });
+        return;
+      }
+      const response = await uploadPartResponse.json();
+      uploadedParts.push(response);
+
+      setFileProgress((prev) => ({
+        ...prev,
+        [file.name]: (i / totalChunks) * 100,
+      }));
+    }
+
+    // Step 3: Complete the multipart upload
+    const completeResponse = await fetch(
+      `${fetchURL}/${encodeURIComponent(
+        file.name
+      )}?action=mpu-complete&uploadId=${uploadId}`,
+      {
+        method: "POST",
+        mode: "cors",
+        body: JSON.stringify({ parts: uploadedParts }),
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    if (!completeResponse.ok) {
+      toast.error("Failed to complete the upload.", {
+        description: "Something went wrong.",
       });
     }
 
-    return await response.json();
+    return true;
   };
 
   const handleDrag = useCallback((e) => {
     e.preventDefault();
     e.stopPropagation();
+    // Manage drag state based on event type
     if (e.type === "dragenter" || e.type === "dragover") {
       setDragActive(true);
     } else if (e.type === "dragleave") {
@@ -50,6 +142,7 @@ export default function UploadButton() {
     e.preventDefault();
     e.stopPropagation();
     setDragActive(false);
+    // Handle file drop event
     if (e.dataTransfer.files && e.dataTransfer.files[0]) {
       onFileChange({ target: { files: e.dataTransfer.files } });
     }
@@ -60,6 +153,7 @@ export default function UploadButton() {
     if (files.length === 0) return;
 
     setLoading(true);
+    // Initialize progress for each selected file
     setFileProgress((prev) => ({
       ...prev,
       ...Object.fromEntries(files.map((file) => [file.name, 0])),
@@ -67,17 +161,36 @@ export default function UploadButton() {
 
     try {
       for (const file of files) {
-        const result = await uploadFile(file);
+        // Generate token for the file upload
+        const token = await TokenGenerator({
+          fileName: encodeURIComponent(file.name),
+          type: "upload",
+        });
+        const result = await uploadFile(file, token);
         if (result) {
+          setFileProgress((prev) => ({ ...prev, [file.name]: 90 }));
+        }
+
+        // Save file metadata after upload
+        const save = await saveFile({
+          name: file.name,
+          type: file.type,
+          size: file.size,
+          lastModified: file.lastModified,
+        });
+
+        if (save) {
           setFileProgress((prev) => ({ ...prev, [file.name]: 100 }));
         }
       }
     } catch (error) {
+      // Handle any errors during the upload process
       toast.error("Something went wrong.", {
         description: error,
       });
     } finally {
-      router.refresh()
+      // Refresh the router and reset states
+      router.refresh();
       setFileProgress({});
       setLoading(false);
     }
