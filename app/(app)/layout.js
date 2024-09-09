8c162f91614a652cd451303a8dc9ecf2bf4d08d3
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-09-09 09:43:57 +0200

    add categories loading

--- app/(app)/layout.js
+++ app/(app)/layout.js
@@ -1,7 +1,9 @@
+import { Suspense } from "react";
+
+import CategoryGrid from "@/components/client/CategoryGrid";
 import ThemeChanger from "@/components/layout/ThemeChanger";
 import SignOutButton from "@/components/SignOutButton";
 import getCategories from "@/utils/getCategories";
-import Link from "next/link";
 
 export default function AppLayout({ children }) {
   return (
@@ -11,7 +13,9 @@ export default function AppLayout({ children }) {
           <span className="text-base inline-block font-medium no-underline text-zinc-800 dark:text-zinc-200">
             Archive Space
           </span>
-          <CategoryList />
+          <Suspense fallback={<CategoriesLoading />}>
+            <Categories />
+          </Suspense>
         </div>
         <div>
           <ThemeChanger />
@@ -24,26 +28,21 @@ export default function AppLayout({ children }) {
   );
 }
 
-async function CategoryList() {
+async function Categories() {
   const categories = await getCategories();
 
+  return <CategoryGrid categories={categories} />;
+}
+
+function CategoriesLoading() {
   return (
     <div className="flex items-center gap-1.5 mt-1">
-      <Link
-        href={`/`}
-        className="bg-zinc-300 dark:bg-zinc-700 px-1.5 py-0.5 rounded text-xs text-zinc-900 dark:text-zinc-100"
-      >
+      <div className="bg-zinc-300 dark:bg-zinc-700 px-1.5 py-0.5 rounded text-xs text-zinc-900 dark:text-zinc-100">
         Home
-      </Link>
-      {categories.map((category) => (
-        <Link
-          key={category}
-          href={`/${category.toLowerCase()}`}
-          className="bg-zinc-300 dark:bg-zinc-700 px-1.5 py-0.5 rounded text-xs text-zinc-900 dark:text-zinc-100"
-        >
-          {category}
-        </Link>
-      ))}
+      </div>
+      <div className="h-5 w-10 bg-zinc-300 dark:bg-zinc-700 rounded animate-pulse"></div>
+      <div className="h-5 w-12 bg-zinc-300 dark:bg-zinc-700 rounded animate-pulse"></div>
+      <div className="h-5 w-14 bg-zinc-300 dark:bg-zinc-700 rounded animate-pulse"></div>
     </div>
   );
 }
