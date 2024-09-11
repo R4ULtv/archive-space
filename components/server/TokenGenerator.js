3a1e01ad399b7c53942a0c5d271348bcbc4dc860
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-09-11 11:54:14 +0200

    add check if the file already exist

--- components/server/TokenGenerator.js
+++ components/server/TokenGenerator.js
@@ -7,16 +7,23 @@ import { auth } from "@/lib/auth";
 export default async function TokenGenerator({ fileName, type }) {
   const session = await auth();
   if (!session) {
-    return { message: "Unauthorized" };
+    return { error: "Unauthorized" };
   }
 
-  const token = crypto.randomBytes(12).toString("hex");
+  const token = crypto.randomBytes(32).toString("hex");
   const client = await clientPromise;
   const db = client.db("production");
 
+  // check if the file already exists
+  const file = await db.collection("files").findOne({ name: fileName });
+  if (file) {
+    return { error: "This file already exists in the cloud." };
+  }
+
+  // insert the token into the database
   await db.collection("tokens").insertOne({
     token: token,
-    file: fileName,
+    file: encodeURIComponent(fileName),
     type: type,
     used: false,
     createdAt: new Date(),
