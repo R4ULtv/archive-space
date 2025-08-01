4e17c362d93488aa8ad850a9a06c62d95fbd70fb
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2025-08-01 10:22:45 +0200

    Add base app layout and styling with Geist fonts

---
+++ app/layout.tsx
@@ -0,0 +1,55 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import { ThemeProvider } from "next-themes";
+import { NuqsAdapter } from "nuqs/adapters/next/app";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  metadataBase: new URL(process.env.HOST_NAME!),
+  title: "Archive Space",
+  description: "A simple archive space build for you by Raul Carini.",
+  openGraph: {
+    title: "Archive Space",
+    description: "A simple archive space build for you by Raul Carini.",
+    url: process.env.HOST_NAME!,
+    images: [
+      {
+        url: `https://www.raulcarini.dev/api/dynamic-og?title=Archive%20Space&description=A%20simple%20archive%20space%20build%20for%20you%20by%20Raul%20Carini.`,
+        width: 843,
+        height: 441,
+      },
+    ],
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${geistMono.variable} ${geistSans.variable} antialiased`}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          disableTransitionOnChange
+        >
+          <NuqsAdapter>{children}</NuqsAdapter>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
