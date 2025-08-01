13313e649fffe916f3760102308fc54db954a079
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2025-08-01 10:07:20 +0200

    Add middleware for session cookie authentication

---
+++ middleware.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getSessionCookie } from "better-auth/cookies";
+
+export async function middleware(request: NextRequest) {
+  const sessionCookie = getSessionCookie(request);
+  console.log("Session cookie:", sessionCookie);
+
+  if (!sessionCookie) {
+    return NextResponse.redirect(new URL("/auth/sign-in", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/"],
+};
