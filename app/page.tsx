033a4ad82d935a38e85d074679c11eda223191dd
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2025-08-01 10:34:51 +0200

    Create main landing page with file management UI

---
+++ app/page.tsx
@@ -0,0 +1,33 @@
+import FileList from "@/components/file-list";
+import FileListSkeleton from "@/components/file-list-skeleton";
+import SignOut from "@/components/sign-out";
+import ThemeSwitch from "@/components/theme-switch";
+import UploadFiles from "@/components/upload-files";
+import { Suspense } from "react";
+
+export default function Home() {
+  return (
+    <div
+      className="max-w-3xl py-16 px-6 mx-auto"
+      style={{ minHeight: "calc(100vh - 45px)" }}
+    >
+      <header className="flex items-start justify-between">
+        <div className="flex flex-col items-start">
+          <span className="text-base inline-block font-medium no-underline font-mono">
+            ARCHIVE SPACE
+          </span>
+        </div>
+        <div className="shrink-0">
+          <ThemeSwitch />
+          <SignOut />
+        </div>
+      </header>
+      <main className="space-y-8 mt-8">
+        <UploadFiles />
+        <Suspense fallback={<FileListSkeleton />}>
+          <FileList />
+        </Suspense>
+      </main>
+    </div>
+  );
+}
