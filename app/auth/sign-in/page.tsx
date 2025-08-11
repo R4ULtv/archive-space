4bb43d82d065bb9585c3c797f6977a4e9c150c07
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2025-08-11 09:00:35 +0200

    feat: add Google Sign-In

--- app/auth/sign-in/page.tsx
+++ app/auth/sign-in/page.tsx
@@ -1,7 +1,13 @@
 "use client";
 
-import { Button } from "@/components/ui/button"
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { signIn } from "@/lib/auth-client";
 import { useState } from "react";
 
@@ -23,16 +29,33 @@ export default function SignInPage() {
         },
       },
     );
-  }
+  };
 
   return (
     <main className="bg-grid-small">
       <div className="absolute pointer-events-none inset-0 md:flex items-center justify-center bg-background [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] hidden"></div>
       <div className="flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
         <div className="flex w-full max-w-sm flex-col gap-6">
-          <a href="#" className="flex items-center gap-2 self-center font-medium">
+          <a
+            href="#"
+            className="flex items-center gap-2 self-center font-medium"
+          >
             <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 16" width="16" height="16"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M9.625 4.75 7.598 2.433a2 2 0 0 0-1.506-.683H3.75a2 2 0 0 0-2 2v8.5a2 2 0 0 0 2 2h2.5m3.375-9.5H1.75m7.875 0h3.625a1 1 0 0 1 1 1v.5m0 5.75H12m0 0H9.75M12 12V9.75M12 12v2.25"></path></svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 16 16"
+                width="16"
+                height="16"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="1.5"
+                  d="M9.625 4.75 7.598 2.433a2 2 0 0 0-1.506-.683H3.75a2 2 0 0 0-2 2v8.5a2 2 0 0 0 2 2h2.5m3.375-9.5H1.75m7.875 0h3.625a1 1 0 0 1 1 1v.5m0 5.75H12m0 0H9.75M12 12V9.75M12 12v2.25"
+                ></path>
+              </svg>
             </div>
             Archive Space
           </a>
@@ -48,10 +71,16 @@ export default function SignInPage() {
                 <form>
                   <div className="grid gap-6">
                     <div className="flex flex-col gap-4">
-                      <Button variant="outline" className="w-full gap-2"
+                      <Button
+                        variant="outline"
+                        className="w-full gap-2"
                         disabled={loading}
-                        onClick={() => handleSignIn("github")}>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                        onClick={() => handleSignIn("github")}
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                        >
                           <path
                             fill="currentColor"
                             d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33s1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2"
@@ -59,8 +88,16 @@ export default function SignInPage() {
                         </svg>
                         Login with Github
                       </Button>
-                      <Button variant="outline" className="w-full" disabled>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                      <Button
+                        variant="outline"
+                        className="w-full gap-2"
+                        disabled={loading}
+                        onClick={() => handleSignIn("github")}
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                        >
                           <path
                             d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
                             fill="currentColor"
@@ -70,22 +107,29 @@ export default function SignInPage() {
                       </Button>
                     </div>
                     <div className="text-center text-sm">
-                      Don&apos;t have an account? {" "}
-                      <a href="mailto:contact@raulcarini.dev" className="underline underline-offset-4">Contact me</a>
+                      Don&apos;t have an account?{" "}
+                      <a
+                        href="mailto:contact@raulcarini.dev"
+                        className="underline underline-offset-4"
+                      >
+                        Contact me
+                      </a>
                     </div>
                   </div>
                 </form>
               </CardContent>
             </Card>
             <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
-              This website is <span className="font-semibold">open-source</span>. You can find the code {" "}
+              This website is <span className="font-semibold">open-source</span>
+              . You can find the code{" "}
               <a href="https://github.com/r4ultv/archive-space" target="_blank">
                 here
-              </a>.
+              </a>
+              .
             </div>
           </div>
         </div>
       </div>
     </main>
-  )
+  );
 }
